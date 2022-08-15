532e377feb38906023247c018bf30312e386e78c
Stella_Ha <sangyeeh@gmail.com>
2022-08-15 23:22:40 +0900

    second commit

--- Netflix_main_page/main.js
+++ Netflix_main_page/main.js
@@ -9,3 +9,19 @@ function toggleSearch() {
     }
     console.log("worked!")
 }
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 5,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
